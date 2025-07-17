2c1e75ecd4b1555b5ebf4cabe9e6148f2634ac42
Xie Yuheng <xyheme@gmail.com>
2025-07-17 11:56:30 +0800

    up

--- fish/conf.d/prompt.fish
+++ fish/conf.d/prompt.fish
@@ -1,15 +1,26 @@
-# function fish_prompt
-#   set last_status $status
-#   printf '\n'
-#   printf '  '
-#   if test $last_status -gt 0
-#     set_color $fish_color_error --bold
-#     printf '%s ' $last_status
-#   end
-#   set_color $fish_color_normal
-#   printf '%s@%s %s' (whoami) (hostname) (pwd)
-#   set_color $fish_color_operator --bold
-#   printf '%s' (__fish_git_prompt)
-#   set_color $fish_color_normal
-#   printf '\n\n'
-# end
+function fish_prompt
+  set last_status $status
+
+  printf '\n'
+  printf '  '
+
+  set_color $fish_color_normal
+  printf '%s@%s %s' (whoami) (hostname) (pwd)
+
+  set_color $fish_color_operator
+  printf '%s' (__fish_git_prompt)
+
+  if test $last_status -ne 0
+    set_color $fish_color_error --bold
+    printf ' [%s]' $last_status
+  end
+
+  set_color $fish_color_normal
+  printf '\n'
+
+  if fish_is_root_user
+    printf '# '
+  else
+    printf '$ '
+  end
+end
