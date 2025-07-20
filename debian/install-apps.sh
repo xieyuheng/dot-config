ec6b6a77f394344fae93e35d4c19fde4170e96ee
Xie Yuheng <xyheme@gmail.com>
2025-07-20 16:59:02 +0800

    up

--- debian/install-apps.sh
+++ debian/install-apps.sh
@@ -2,20 +2,19 @@ sudo apt install \
     fish fzf emacs ranger \
     rsync linux-headers-amd64 zfsutils-linux \
     git tig nodejs npm \
-    mpv vlc ffmpeg i3 i3status tree wget \
+    mpv ffmpeg i3 i3status tree wget \
     tlp \
-    alacritty rxvt-unicode \
+    rxvt-unicode \
     clang valgrind cloc info \
     libx11-dev libsdl2-dev \
     proxychains4 vim zsh dkms ncdu \
-    unifont fonts-noto fonts-linuxlibertine \
+    unifont fonts-noto \
     fastfetch htop btop gnome-power-manager \
-    rofi feh scrot xbindkeys xcwd \
+    feh scrot xbindkeys xcwd \
     flameshot network-manager-applet cbatticon \
     apt-file xclip light \
     ugrep silversearcher-ag \
     zathura zathura-djvu zathura-pdf-poppler zathura-ps \
-    mupdf djview4 calibre \
-    stardict goldendict \
-    chromium \
-    gimp blender
+    djview4 calibre \
+    goldendict \
+    chromium
