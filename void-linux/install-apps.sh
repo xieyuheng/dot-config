95b6df5296d67d82aca6c692ef797bbbf8f31f1f
Xie Yuheng <xyheme@gmail.com>
2025-07-23 13:58:42 +0800

    up

--- void-linux/install-apps.sh
+++ void-linux/install-apps.sh
@@ -1,22 +1,21 @@
 sudo xbps-install \
-    rsync fish-shell starship emacs-gtk3 ranger zfs xz \
+    rsync fish-shell emacs-gtk3 ranger zfs xz \
     git tig nodejs \
-    mpv vlc ffmpeg i3 i3status tree wget curl \
-    alacritty xfce4-terminal \
+    mpv ffmpeg i3 i3status tree wget curl \
+    alacritty \
     clang valgrind cloc gdb libsanitizer libsanitizer-devel \
     libX11-devel SDL2-devel \
     proxychains-ng vim zsh ncdu \
     noto-fonts-cjk noto-fonts-ttf noto-fonts-ttf-extra \
     fastfetch htop btop \
-    rofi feh scrot xclip xbindkeys \
+    dmenu feh scrot xclip xbindkeys \
     light brillo flameshot network-manager-applet cbatticon \
     ugrep the_silver_searcher \
-    okular mupdf zathura zathura-djvu zathura-pdf-mupdf zathura-ps \
+    zathura zathura-djvu zathura-pdf-mupdf zathura-ps \
     djview calibre \
     goldendict \
     amber onefetch \
     xorg xorg-fonts \
     sof-firmware alsa-firmware pipewire alsa-pipewire alsa-utils \
-    chromium firefox telegram-desktop \
-    fcitx5 fcitx5-chinese-addons fcitx5-cloudpinyin fcitx5-configtool fcitx5-gtk fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt fcitx5-qt5 fcitx5-qt6 fcitx5-rime \
-    gimp blender
+    chromium firefox \
+    fcitx5 fcitx5-chinese-addons fcitx5-cloudpinyin fcitx5-configtool fcitx5-gtk fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt fcitx5-qt5 fcitx5-qt6 fcitx5-rime
