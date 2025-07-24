1501d4796af128f956274df8e82f9ca6cae06b6e
Xie Yuheng <xyheme@gmail.com>
2025-07-24 12:43:10 +0800

    up

--- void-linux/install-apps.sh
+++ void-linux/install-apps.sh
@@ -16,6 +16,6 @@ sudo xbps-install \
     goldendict \
     amber onefetch \
     xorg xorg-fonts \
-    sof-firmware alsa-firmware pipewire alsa-pipewire alsa-utils \
+    alsa-utils \
     chromium firefox \
     fcitx5 fcitx5-chinese-addons fcitx5-cloudpinyin fcitx5-configtool fcitx5-gtk fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt fcitx5-qt5 fcitx5-qt6 fcitx5-rime
