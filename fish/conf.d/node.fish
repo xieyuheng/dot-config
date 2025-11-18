8cca45eb42c8ce0d96a2f94c5f5c536ff93f2937
Xie Yuheng <xyheme@gmail.com>
2025-11-18 20:26:40 +0800

    up

--- fish/conf.d/node.fish
+++ fish/conf.d/node.fish
@@ -1,6 +1,5 @@
-set PATH $PATH $HOME/.bun/bin
 set -x N_PREFIX $HOME/.n
 set PATH $N_PREFIX/bin $PATH
 
 set PATH $HOME/.local/share/pnpm $PATH
-set -x YARN_CACHE_FOLDER $HOME/.yarn-cache
+set PATH $HOME/.bun/bin $PATH
