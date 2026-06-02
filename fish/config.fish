be8f599571ac0c10f0a35b1151048f87392e89cf
Xie Yuheng <xyheme@gmail.com>
2026-06-03 07:37:25 +0800

    revert EMACS_NATIVE_COMPILATION_CACHE_DIR changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,1 @@
 
-# pnpm
-set -gx PNPM_HOME "/home/xyh/.local/share/pnpm"
-if not string match -q -- "$PNPM_HOME/bin" $PATH
-  set -gx PATH "$PNPM_HOME/bin" $PATH
-end
-# pnpm end
-
-# opencode
-fish_add_path /home/xyh/.opencode/bin
-
-# emacs
-set -x EMACS_NATIVE_COMPILATION_CACHE_DIR "$HOME/.cache/emacs/eln-cache/"
