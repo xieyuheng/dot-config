2155ce4c61fa9faf9bfb10239cc5b3e934d189b8
Xie Yuheng <xyheme@gmail.com>
2026-07-23 03:05:05 +0800

    up

---
+++ fcitx5/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp profile ~/.config/fcitx5/profile
---
+++ rime/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp default.custom.yaml ~/.local/share/fcitx5/rime/
+cp luna_pinyin.custom.yaml ~/.local/share/fcitx5/rime/
+cp terra_pinyin.custom.yaml ~/.local/share/fcitx5/rime/
