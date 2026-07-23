2cba12ed60ef930672ee104e2e41a170cfaad799
Xie Yuheng <xyheme@gmail.com>
2026-07-23 13:30:48 +0800

    up

--- fcitx5/install.sh
+++ fcitx5/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 cp profile ~/.config/fcitx5/profile
+
+if [ ! -d ~/.local/share/fcitx5/themes/fcitx5-dark ]; then
+  git clone https://github.com/evansan/fcitx5-dark ~/.local/share/fcitx5/themes/fcitx5-dark
+fi
--- rime/install.sh
+++ rime/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cp default.custom.yaml ~/.local/share/fcitx5/rime/
-cp luna_pinyin.custom.yaml ~/.local/share/fcitx5/rime/
 cp terra_pinyin.custom.yaml ~/.local/share/fcitx5/rime/
