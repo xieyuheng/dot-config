0ae29fb18eec2a1837522c8b368a53c4173044cb
Xie Yuheng <xyheme@gmail.com>
2026-07-25 02:25:34 +0800

    up

--- fcitx5/install-theme.sh
+++ fcitx5/install-theme.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-dir="$HOME/.local/share/fcitx5/themes/fcitx5-dark"
-
-if [ -d "$dir/.git" ]; then
-  git -C "$dir" pull
-else
-  git clone https://github.com/evansan/fcitx5-dark "$dir"
+if [ ! -d ~/.local/share/fcitx5/themes/fcitx5-dark ]; then
+  git clone https://github.com/evansan/fcitx5-dark ~/.local/share/fcitx5/themes/fcitx5-dark
 fi
-
-echo "ok"
